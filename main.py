586846a7844e6c152aaa5db7b6f0744850e7e7bc
test <sebastian.kuppa@web.de>
2022-12-16 19:14:13 +0800

    added Discriminator class

--- main.py
+++ main.py
@@ -26,3 +26,23 @@ plt.show()
 batch_size = 32
 train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
 
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(2, 256),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(64, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.model(x)
