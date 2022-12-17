fb6dec4d243abf992ca0faf411551dc1308f6a8d
test <sebastian.kuppa@web.de>
2022-12-17 18:28:42 +0800

    init Discriminator class, created Generator() class

--- main.py
+++ main.py
@@ -46,3 +46,26 @@ class Discriminator(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.model(x)
+
+
+discriminator = Discriminator()
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(2, 16),
+            nn.ReLU(),
+            nn.Linear(16, 32),
+            nn.ReLU(),
+            nn.Linear(32, 2),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.model(x)
+
+
+generator = Generator()
+
+
