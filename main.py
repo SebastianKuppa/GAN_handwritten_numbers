fd1ccdf2b1956987e7370f900440e2ae105e2287
test <sebastian.kuppa@web.de>
2022-12-18 17:37:14 +0800

    added visualization of trained generator results

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ train_data[:, 1] = torch.sin(train_data[:, 0])
 train_labels = torch.zeros(train_data_length)
 train_set = [(train_data[i], train_labels[i]) for i in range(train_data_length)]
 
-plt.plot(train_data[:, 0], train_data[:, 1], ".")
-plt.show()
+# plt.plot(train_data[:, 0], train_data[:, 1], ".")
+# plt.show()
 
 batch_size = 32
 train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
@@ -110,3 +110,8 @@ for epoch in range(num_epochs):
             print(f"Epoch: {epoch} Loss D.: {loss_discriminator}")
             print(f"Epoch: {epoch} Loss G.: {loss_generator}")
 
+latent_space_samples = torch.randn((100, 2))
+generated_samples = generator(latent_space_samples)
+generated_samples = generated_samples.detach()
+plt.plot(generated_samples[:, 0], generated_samples[:, 1], ".")
+plt.show()
