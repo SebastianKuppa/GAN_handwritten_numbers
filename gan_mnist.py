e7a267d4f7b9c8f911f8bbb612339a6a06be8c5b
test <sebastian.kuppa@web.de>
2022-12-21 17:43:21 +0800

    downloaded mnist dataset from torchvision, visualized handwritten digits for 1 batch from dataloader

--- gan_mnist.py
+++ gan_mnist.py
@@ -1,4 +1,5 @@
 import torch
+from torch.utils.data import DataLoader
 import torch.nn as nn
 
 import torchvision
@@ -7,4 +8,29 @@ import torchvision.transforms as transforms
 import math
 import matplotlib.pyplot as plt
 
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
+torch.manual_seed(111)
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+transform = transforms.Compose(
+    [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+)
+
+train_set = torchvision.datasets.MNIST(root=".", train=True, download=True, transform=transform)
+
+batch_size = 32
+train_loader = DataLoader(train_set, batch_size, shuffle=True)
+
+real_samples, mnist_labels = next(iter(train_loader))
+for i in range(16):
+    ax = plt.subplot(4, 4, i+1)
+    plt.imshow(real_samples[i].reshape(28, 28), cmap="Greys_r")
+    plt.xticks([])
+    plt.yticks([])
+plt.show()
